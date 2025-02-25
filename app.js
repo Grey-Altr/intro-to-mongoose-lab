f2fa8f766aca7265db98dd3171a7fcdbefb98846
GreyA <grey.altaer@gmail.com>
2025-02-25 09:39:52 -0800

    welcome funtion created in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ dotenv.config();
 const mongoose = require('mongoose');
 const Customer = require('./models/customer');
 
-// const prompt = require('prompt-sync')();
+const prompt = require('prompt-sync')();
 
 // const username = prompt('What is your name? ');
 
@@ -26,3 +26,22 @@ const runQueries = async () => {
 };
 
 connect()
+
+
+const welcome = () => {
+    prompt(`Welcome to the CRM
+
+What would you like to do?
+
+  1. Create a customer
+  2. View all customers
+  3. Update a customer
+  4. Delete a customer
+  5. quit
+
+Number of action to run: 
+# user inputs 3
+`);
+};
+
+welcome();

8628636f0c324c8b856c4c3991bb6acdb269891c
GreyA <grey.altaer@gmail.com>
2025-02-25 13:07:39 -0800

    deleteCustomer() complete

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ const connect = async () => {
 
 const runQueries = async () => {
     console.log('Queries running.');
+    await createCustomer();
+    await viewAll();
+    await updateCustomer();
 };
 
 connect()
@@ -48,6 +51,10 @@ const welcome = () => {
             createCustomer();
         } else if (action === '2') {
             viewAll();
+        } else if (action === '3') {
+            updateCustomer();
+        } else if (action === '4') {
+            deleteCustomer();
         } else if (action === '5') {
             quitApp();
         } else {
@@ -63,7 +70,7 @@ const createCustomer = async () => {
     const age = prompt('Enter customer age: ');
 
     const customer = new Customer({ name, age });
-    await customer.create();
+    await customer.save();
 
     console.log('New customer created.')
 };
@@ -76,12 +83,32 @@ const viewAll = async () => {
     });
 };
 
-const updateCustomer = () => {
+const updateCustomer = async () => {
+    const name = prompt('Enter name of customer to update: ');
+    const customer = await Customer.findOne({ name });
+    
+    if (!customer) {
+        console.log('Customer not found.');
+        return;
+    };
 
+    const updateName = prompt(`Enter new name for (${customer.name}): `);
+    const updateAge = prompt(`Enter new age for (${customer.age}): `);
+
+    await customer.save();
+
+    console.log(`Customer ${customer.name} updated.`);
 };
 
-const deleteCustomer = () => {
+const deleteCustomer = async () => {
+    const name = prompt('Enter name of customer to delete: ');
+    const customer = await Customer.findOneAndDelete({ name });
 
+    if (!customer) {
+        console.log('Customer not found.');
+    } else {
+        console.log(`Customer ${customer.name} deleted.`);
+    };
 };
 
 const quitApp = () => {
